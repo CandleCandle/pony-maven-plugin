8b5df25272ab7f546aed66f285a394eba3ac31ce
Andrew Wheat <andrew.wheat@bbc.co.uk>
2016-08-13 23:06:43 +0100

    start adding logic.

--- src/main/java/uk/me/candle/maven/pony/plugin/PonyCleanMojo.java
+++ src/main/java/uk/me/candle/maven/pony/plugin/PonyCleanMojo.java
@@ -1,5 +1,7 @@
 package uk.me.candle.maven.pony.plugin;
 
+import java.io.File;
+import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -7,15 +9,25 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name="compile")
-public class PonyCompileMojo extends AbstractMojo {
+@Mojo(name="clean")
+public class PonyCleanMojo extends AbstractMojo {
 
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
 	private MavenProject project;
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("maybe compiling " + project.getArtifactId());
+		File target = new File(project.getBuild().getDirectory());
+		getLog().info("cleaning " + target.getAbsolutePath());
+		try {
+			Process p = new ProcessBuilder("rm", "-rvf", target.getAbsolutePath())
+					.redirectErrorStream(true)
+					.inheritIO()
+					.start();
+			p.waitFor();
+		} catch (IOException | InterruptedException e) {
+			throw new MojoExecutionException("Failed to clean", e);
+		}
 	}
 }
 // vi: sw=4 ts=4 sts=4 noet
